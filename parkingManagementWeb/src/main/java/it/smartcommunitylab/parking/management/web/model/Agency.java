e000c4465f1384efad18a6c2c2fa27e7996dfe75
Mattia Bortolamedi <regolo85@gmail.com>
2016-06-24 13:53:41 +0200

    Updated app before repository switch

---
+++ parkingManagementWeb/src/main/java/it/smartcommunitylab/parking/management/web/model/Agency.java
@@ -0,0 +1,33 @@
+package it.smartcommunitylab.parking.management.web.model;
+
+public class Agency {
+
+	private String id;
+	private String name;
+	private String description;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
