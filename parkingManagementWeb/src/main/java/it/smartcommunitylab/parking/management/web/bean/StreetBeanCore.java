30b0875bee8942a72a6776cb9bde6db21c38b41f
Mattia Bortolamedi <regolo85@gmail.com>
2016-11-10 15:13:51 +0100

    New files for rest api

---
+++ parkingManagementWeb/src/main/java/it/smartcommunitylab/parking/management/web/bean/StreetBeanCore.java
@@ -0,0 +1,72 @@
+package it.smartcommunitylab.parking.management.web.bean;
+
+import java.util.List;
+import java.util.Map;
+
+public class StreetBeanCore {
+
+	private String id;
+	private String id_app;
+	private String streetReference;
+	private Integer slotNumber;
+	private String rateAreaId;
+	private List<String> zones;
+	private Map<String, Object> occupancyData; // Attribute used in new rest API for planner
+
+	public String getId() {
+		return id;
+	}
+
+	public String getId_app() {
+		return id_app;
+	}
+
+	public String getStreetReference() {
+		return streetReference;
+	}
+
+	public Integer getSlotNumber() {
+		return slotNumber;
+	}
+
+	public String getRateAreaId() {
+		return rateAreaId;
+	}
+
+	public List<String> getZones() {
+		return zones;
+	}
+
+	public Map<String, Object> getOccupancyData() {
+		return occupancyData;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setId_app(String id_app) {
+		this.id_app = id_app;
+	}
+
+	public void setStreetReference(String streetReference) {
+		this.streetReference = streetReference;
+	}
+
+	public void setSlotNumber(Integer slotNumber) {
+		this.slotNumber = slotNumber;
+	}
+
+	public void setRateAreaId(String rateAreaId) {
+		this.rateAreaId = rateAreaId;
+	}
+
+	public void setZones(List<String> zones) {
+		this.zones = zones;
+	}
+
+	public void setOccupancyData(Map<String, Object> occupancyData) {
+		this.occupancyData = occupancyData;
+	}
+	
+}
