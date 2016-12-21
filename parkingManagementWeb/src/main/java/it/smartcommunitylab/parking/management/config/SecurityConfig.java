9ab9a7ce69b9c60de913eba5b837e24f08651013
gioadami <giordano.adami@gmail.com>
2016-12-21 10:39:20 +0100

    Cleanup.

--- parkingManagementWeb/src/main/java/it/smartcommunitylab/parking/management/config/SecurityConfig.java
+++ parkingManagementWeb/src/main/java/it/smartcommunitylab/parking/management/config/SecurityConfig.java
@@ -14,170 +14,53 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-//@EnableWebSecurity
-//@Order(value = 0)
 public class SecurityConfig {
 
 	@Autowired
-	private YamlUserDetailsService yamlUserDetailsService;	
-	
-//	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.userDetailsService(yamlUserDetailsService);
-//		auth.authenticationProvider(getYamlAuthenticationProvider());
+	private YamlUserDetailsService yamlUserDetailsService;
+
+	@Autowired
+	public void configAuthBuilder(AuthenticationManagerBuilder builder) throws Exception {
+		builder.userDetailsService(yamlUserDetailsService);
 	}
 
-	 @Autowired
-	    public void configAuthBuilder(AuthenticationManagerBuilder builder) throws Exception {
-	        builder.userDetailsService(yamlUserDetailsService); 
-	    }	
-	
-	
-//	@Override
-//	public void configure(WebSecurity web) throws Exception {
-//		web.ignoring().antMatchers("/css/**","/fonts/**","/js/**","/lib/**", "/imgs/**");
-//	}	
-	
-	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.csrf().disable();
-//		http.rememberMe();	
-//		
-//		http.authorizeRequests().antMatchers("/**").fullyAuthenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();	
-//	}	
-	
-//	@Bean
-//	public AbstractUserDetailsAuthenticationProvider getYamlAuthenticationProvider() {
-////		DaoAuthenticationProvider daoAP = new DaoAuthenticationProvider();
-//		YamlAuthenticationProvider yamlAP = new YamlAuthenticationProvider();
-//		yamlAP.setUserDetailsService(yamlUserDetailsService);
-//		return yamlAP;
-//	}	
-	
-    @Configuration
-    @Order(30)                                                        
-    public static class BasicAuthSecurityConfig extends WebSecurityConfigurerAdapter {
-    
-    	@Override
-    	protected void configure(HttpSecurity http) throws Exception {
-    		http.csrf().disable();
-//    		http.rememberMe();	
-    		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    		
-//    		http.antMatcher("/data-mgt/**").authorizeRequests().antMatchers("/data-mgt/**").fullyAuthenticated().and().httpBasic();
-    		http.antMatcher("/data-mgt/**").authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
-    		
-    	}    	
-    }		 
-	
-    
+	@Configuration
+	@Order(30)
+	public static class BasicAuthSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-//	OK    
-@Configuration
-@Order(50)                                                        
-public static class ConsoleSecurityConfig extends WebSecurityConfigurerAdapter {
+			http.antMatcher("/data-mgt/**").authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-//		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//		http.rememberMe();	
-//		http.sessionManagement().invalidSessionUrl("/error.html").sessionFixation().newSession();
-		
-		http.authorizeRequests().antMatchers("/home", "/rest/**", "/auxiliary/rest/**", "/dashboard/rest/**").fullyAuthenticated()
-		.and().formLogin().loginPage("/login").permitAll()
-//		.and().logout().logoutUrl("/logout").permitAll();
-		.and().logout().deleteCookies("JSESSIONID").clearAuthentication(true).logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/home");
-	}    	
-}	    
-    
-//@Configuration
-//@Order(52)                                                        
-//public static class NoSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.csrf().disable();
-////		http.rememberMe();	
-//		http.sessionManagement().invalidSessionUrl("/error.html").sessionFixation().newSession();
-//		
-//		http.authorizeRequests().antMatchers("/logout").fullyAuthenticated().anyRequest().permitAll();
-//	}    	
-//}	 
+		}
+	}
 
+	@Configuration
+	@Order(50)
+	public static class ConsoleSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//@Configuration
-//@Order(51)                                                        
-//public static class EverythingSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.csrf().disable();
-//		http.rememberMe();	
-//		http.sessionManagement().invalidSessionUrl("/error.html").sessionFixation().newSession();
-//		
-//		http.authorizeRequests().antMatchers("/home", "/rest/**", "/auxiliary/rest/**", "/dashboard/rest/**").fullyAuthenticated().and().formLogin().loginPage("/login").defaultSuccessUrl("/home").failureUrl("/login?error=true").permitAll().and().logout().deleteCookies("JSESSIONID").clearAuthentication(true).permitAll();
-//	}    	
-//}
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			http.authorizeRequests().antMatchers("/home", "/rest/**", "/auxiliary/rest/**", "/dashboard/rest/**").fullyAuthenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+					.deleteCookies("JSESSIONID").clearAuthentication(true).logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/home");
+		}
+	}
 
-    
-//    @Configuration
-//    @Order(40)                                                        
-//    public static class RestSecurityConfig extends WebSecurityConfigurerAdapter {
-//    
-//    	@Override
-//    	protected void configure(HttpSecurity http) throws Exception {
-//    		http.csrf().disable();
-//    		http.rememberMe();	
-//    		
-//    		http.authorizeRequests().antMatchers("/home", "/rest/**", "/auxiliary/rest/**", "/dashboard/rest/**").fullyAuthenticated(); //.and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
-//    	}    	
-//    }    
-//    
-//    @Configuration
-//    @Order(50)                                                        
-//    public static class ConsoleSecurityConfig extends WebSecurityConfigurerAdapter {
-//    
-//    	@Override
-//    	protected void configure(HttpSecurity http) throws Exception {
-//    		http.csrf().disable();
-//    		http.rememberMe();	
-//    		
-//    		http.authorizeRequests().antMatchers("/**").fullyAuthenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
-//    	}    	
-//    }  
+	@Configuration
+	@Order(60)
+	public static class NoSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    
-//  @Configuration
-//  @Order(40)                                                        
-//  public static class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
-//  
-//  	@Override
-//  	protected void configure(HttpSecurity http) throws Exception {
-//  		http.csrf().disable();
-//  		http.rememberMe();	
-//  		
-//  		http.antMatcher("/login").authorizeRequests().anyRequest().permitAll();
-//  	}    	
-//  }    
-//  
-//  
-////		OK    
-//  @Configuration
-//  @Order(50)                                                        
-//  public static class ConsoleSecurityConfig extends WebSecurityConfigurerAdapter {
-//  
-//  	@Override
-//  	protected void configure(HttpSecurity http) throws Exception {
-//  		http.csrf().disable();
-//  		http.rememberMe();	
-//  		
-//  		http.authorizeRequests().antMatchers("/home").fullyAuthenticated().and().formLogin().loginPage("/login").defaultSuccessUrl("/home").permitAll().and().logout().permitAll();
-//  	}    	
-//  }	    
-    
-    
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+			http.authorizeRequests().antMatchers("/api/**").anonymous().anyRequest().permitAll();
+		}
+	}
+
 }
